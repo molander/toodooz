49f25192c51c20a45d0187f48cc2dba5aa7f4818
molander <noreply>
2015-07-09 06:50:23 +0000

    Add component leftnavbar

---
+++ ui/leftnavbar.reel/leftnavbar.js
@@ -0,0 +1,16 @@
+/**
+ * @module ui/leftnavbar.reel
+ */
+var Component = require("montage/ui/component").Component;
+
+/**
+ * @class Leftnavbar
+ * @extends Component
+ */
+exports.Leftnavbar = Component.specialize(/** @lends Leftnavbar# */ {
+    constructor: {
+        value: function Leftnavbar() {
+            this.super();
+        }
+    }
+});
